32ebe5cbca704a8e9a7f1df2577bcbbaab74b278
Victor Alfaro <142273132+gitonina@users.noreply.github.com>
2023-11-11 23:00:56 -0300

    Update Laboratorio5.py

--- Laboratorio5.py
+++ Laboratorio5.py
@@ -3,10 +3,7 @@ import psycopg2.extras
 import csv
 import re
 
-conn = psycopg2.connect(host="cc3201.dcc.uchile.cl",
-    database="cc3201",
-    user="cc3201",
-    password="j'<3_cc3201", port="5440")
+conn = psycopg2.connect()
 
 cur = conn.cursor()
 
